149bb54a20e49bf24b1077406ecf3f6965dd83c6
Mohamed Nuski Nawfer <42333054+thenuskinawfer@users.noreply.github.com>
2024-12-29 20:40:12 +0530

    Adding Javascript Beginner Stuff

---
+++ JavascriptBeginner/JavascriptBeginner/HelloWorld.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log(Date());
+
+function AlertFunction() {
+    document.getElementById("Paragraph1").innerHTML = "New Text";
+    alert("Hello, World!");
+    document.getElementById("Bulb").src="pic_bulboff.gif";
+    document.getElementById("Paragraph1").style.fontSize = "35px";
+}
