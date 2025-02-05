f3e52599486a1f60cdbadc65f602bdf778c4a99e
Nuski Nawfer <nuskinawfer@outlook.com>
2025-02-05 09:35:21 +0530

    Training

--- JavascriptBeginner/JavascriptBeginner/JavascriptSyntaxTraining.js
+++ JavascriptBeginner/JavascriptBeginner/JavascriptSyntaxTraining.js
@@ -1,2 +1,16 @@
-let greeting = "Hello !";
-console.log(greeting);
+const greeting = 'Hello';
+
+function CreateNewHuman(HumanAttributes) {
+    return {
+        FirstName: function() { return HumanAttributes[0]; },
+        LastName: function() { return HumanAttributes[1]; },
+        DateOfBirth: function() { return new Date(HumanAttributes[2]); },
+        CreateGreeting: function() {
+            return greeting + " " + this.FirstName() + " " + this.LastName() + ". Today is " + this.DateOfBirth();
+        }
+    };
+}
+
+const HumanOneAttributes = ['Nuski', 'Nawfer', '2001-01-01'];
+const HumanOne = CreateNewHuman(HumanOneAttributes);
+console.log(HumanOne.CreateGreeting());
