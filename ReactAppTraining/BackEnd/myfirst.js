a4513e0011a52d924fa780c854582d4186c135dd
Nuski Nawfer <42333054+thenuskinawfer@users.noreply.github.com>
2025-02-12 10:43:09 +0530

    Added mssql and mysql

--- ReactAppTraining/BackEnd/myfirst.js
+++ ReactAppTraining/BackEnd/myfirst.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url')
 const DisplayDate = require('./myfirstmodule')
 const readfile = require('fs')
+const mssql = require('mssql')
 
 http.createServer(function (req,res) {
     res.writeHead(200,"Status Message Successfull"); //This is the request header of the HTTP Request, you can view this in Inspect
@@ -14,9 +15,31 @@ http.createServer(function (req,res) {
     //const closingmessage = divide.year;
     //res.end(closingmessage); Splitting the Query String
 
-    readfile.readfile('myfirsthtml.html', function(err,data) {
-        res.write(data);
-    })
+    // readfile.readfile('myfirsthtml.html', function(err,data) {
+    //     res.write(data);
+    // })
 
+    const config = {
+        user:'nuski',
+        password:'JungleFever@19911',
+        server:'mytrainingdatabase.database.windows.net',
+        database:'MyTrainingDatabase',
+        options: {
+            encrypt:true,
+            trustUserCertificate:false
+
+        }
+    }
+
+    async function newConnection(){
+        try {
+            let connection = await mssql.connect(config)
+            console.log(`Connected to MySQL : ${connection}`)
+        } catch (err) {
+            console.error('Connection Failed',err)
+        }
+    }
+
+    newConnection()
 
 }).listen(8080);
