f91fddf74eca7a321b9f5c1584a27b7bfcb056c2
Nuski Nawfer <42333054+thenuskinawfer@users.noreply.github.com>
2025-02-12 13:07:31 +0530

    File Name Casing Change

--- ReactAppTraining/BackEnd/ProjectUtilityComponents/executeQuery.js
+++ ReactAppTraining/BackEnd/ProjectUtilityComponents/executeQuery.js
@@ -1,5 +1,5 @@
-const mssql = require ('mssql');
-const mssqlconnector = require ('./mssqlconnector');
+const mssql = require ('mssql')
+const mssqlconnector = require('../ProjectUtilityComponents/MSSQLConnector')
 
 async function executeSelectQuery() {
     try {}
