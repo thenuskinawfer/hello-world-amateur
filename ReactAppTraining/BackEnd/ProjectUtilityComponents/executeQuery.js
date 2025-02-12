3f55b9d5d7323b3ada6326eafa622b70a6c7dff4
Nuski Nawfer <42333054+thenuskinawfer@users.noreply.github.com>
2025-02-12 14:18:28 +0530

    Bug Fixes and Error Handling

--- ReactAppTraining/BackEnd/ProjectUtilityComponents/executeQuery.js
+++ ReactAppTraining/BackEnd/ProjectUtilityComponents/executeQuery.js
@@ -1,6 +1,16 @@
 const mssql = require ('mssql')
-const mssqlconnector = require('./MSSQLConnector')
+const mssqlconnector = require ('./mssqlconnector')
 
-async function executeSelectQuery() {
-    try {}
+exports.executeQuery = async function executeQuery(query) {
+    try {
+        let connection = mssqlconnector.connectionMessage
+        let newRequest = (await connection).request()
+        let newQuery = newRequest.query(query)
+        console.log((await newQuery).recordset)
+        ;(await connection).close()
+    } catch (err) {
+        console.error('Error',err)
+    }
 }
+
+exports.executeQuery('SELECT * FROM StudentTable')
--- ReactAppTraining/BackEnd/ProjectUtilityComponents/mssqlconnector.js
+++ ReactAppTraining/BackEnd/ProjectUtilityComponents/mssqlconnector.js
@@ -1,30 +1,15 @@
 const mssql = require ('mssql')
-const config = require('../ProjectConfigurationFiles/appsettings.json')
+const config = require ('../ProjectConfigurationFiles/appsettings.json')
 
-let connectionMessage = mssql.connect(config.databaseConnectionString)
+exports.connectionMessage = mssql.connect(config.databaseConnectionString)
 
-//exports.connect =
 async function connectToDatabase() {
     try {
-        console.log(`Connected to Database Server : ${connectionMessage}`)
-        return(`Connected to Database Server : ${connectionMessage}`)
+        const connectionStatus = await exports.connectionMessage
+        console.log(`Connected to Database Server ${connectionStatus}`)
     } catch(err) {
-        console.log(`Unable to connect to Database : ${connectionMessage}`)
-        return(`Unable to connect to Database : ${connectionMessage}`)
+        console.error(`Unable to connect to Database : ${err}`)
     }
 }
-
-async function executeQuery() {
-    try {
-        const initiateRequest = (await connectionMessage).request()
-        const newQuery = initiateRequest.query('EXEC dbo.SelectAllStudents')
-        const queryResult = (await newQuery).recordset
-        console.log(queryResult)
-    } catch (err) {
-        console.log('Error')
-    }
-}
-
 
 connectToDatabase()
-executeQuery()
