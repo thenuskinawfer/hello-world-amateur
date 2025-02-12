6d87787388b63b340a43437bb70235f910519065
Nuski Nawfer <nuskinawfer@outlook.com>
2025-02-12 14:48:04 +0530

    Commit from .NET Training

---
+++ .NETBeginner/NETSyntaxTraining/NETSyntaxTraining.cs
@@ -0,0 +1,4 @@
+Console.WriteLine('N');
+Console.WriteLine(100);
+Console.WriteLine("Alex");
+Console.WriteLine(0.25F);
---
+++ .NETBeginner/NETSyntaxTraining/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine('N');
+Console.WriteLine(100);
+Console.WriteLine("Alex");
+Console.WriteLine(0.25F);
+Console.WriteLine('N');
+Console.WriteLine(100);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+string lastName = " the Builder";
+Console.WriteLine($"{firstName}{lastName}");
+
+var firstName = "Bob";
+var messageCount = 3;
+var currentTemperature = 34.4F;
+
+var displayMessage = $"Hello, {firstName}! You have {messageCount} messages in your inbox. The temperature is {currentTemperature} celcius.";
+Console.WriteLine(displayMessage);
